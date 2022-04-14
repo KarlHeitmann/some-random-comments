8bc4462ee71e25cbc1833c9e270720e53399d781
Karl Heitmann <khf1988@gmail.com>
2022-04-14 18:42:19 -0400

    it displays comments once an api request is done from the front-end

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,8 +14,40 @@ const generateComment = async () => {
 
 document.addEventListener('DOMContentLoaded', async () => {
   console.log("Hello World");
-  const r = await generateComment()
-  console.log(r)
+  const { comments } = await generateComment()
+  console.table(comments)
+  comments.forEach(comment => {
+    /*
+    // console.log(comment)
+    // const row = '<div class="column">' +
+    //   `<figure class="image is-64x64"><img src="./assets/images/${comment.avatar}.jpg" class="is-rounded"></figure>` +
+    //   `${comment.content}` +
+    // '</div>'
+
+    const row = '' +
+      `<figure class="image is-64x64"><img src="./assets/images/${comment.avatar}.jpg" class="is-rounded"></figure>` +
+      `${comment.content}` +
+    '</div>'
+
+    // const row = '' +
+    //   `<div><figure class=" image is-64x64"><img src="./assets/images/${comment.avatar}.jpg" class="is-rounded"></figure></div class="column is-one-fifth">` +
+    //   `<p class="column is-four-fifth">${comment.content}</p>`
+    // ''
+    console.log(row)
+    */
+  //   const row = `<div class="columns">    <figure class="image is-64x64">
+  //   <img src="./assets/images/${comment.avatar}.jpg" alt="person 1" class="is-rounded">
+  // </figure>
+  // <p>${comment.content}</p></div>`
+    const row = `<div class="columns">
+      <figure class="image is-64x64 column is-1">
+      <img src="./assets/images/${comment.avatar}.jpg" alt="person 1" class="is-rounded">
+      </figure>
+      <p>${comment.content}</p></div>
+    `
+    document.querySelector('#comments').innerHTML += row
+  });
+  // console.log(comments)
   console.log("fin")
   document.querySelector('#comments')
 })
