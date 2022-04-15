2d46651fc2747f46ffd52ef298ab864e076af6bb
Karl Heitmann <khf1988@gmail.com>
2022-04-15 02:25:44 -0400

    some refactor done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-const generateComment = async () => {
+const requestComments = async () => {
   var requestOptions = {
     method: 'GET',
     redirect: 'follow',
@@ -12,39 +12,21 @@ const generateComment = async () => {
     .then(response => response.json())
 }
 
+const generateRowHtml = ({content, avatar}) => {
+  return `<div class="columns">
+      <figure class="image is-64x64 column is-1">
+      <img src="./assets/images/${avatar}.jpg" alt="person 1" class="is-rounded">
+      </figure>
+      <p>${content}</p></div>
+    `
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   console.log("Hello World");
-  const { comments } = await generateComment()
+  const { comments } = await requestComments()
   console.table(comments)
   comments.forEach(comment => {
-    /*
-    // console.log(comment)
-    // const row = '<div class="column">' +
-    //   `<figure class="image is-64x64"><img src="./assets/images/${comment.avatar}.jpg" class="is-rounded"></figure>` +
-    //   `${comment.content}` +
-    // '</div>'
-
-    const row = '' +
-      `<figure class="image is-64x64"><img src="./assets/images/${comment.avatar}.jpg" class="is-rounded"></figure>` +
-      `${comment.content}` +
-    '</div>'
-
-    // const row = '' +
-    //   `<div><figure class=" image is-64x64"><img src="./assets/images/${comment.avatar}.jpg" class="is-rounded"></figure></div class="column is-one-fifth">` +
-    //   `<p class="column is-four-fifth">${comment.content}</p>`
-    // ''
-    console.log(row)
-    */
-  //   const row = `<div class="columns">    <figure class="image is-64x64">
-  //   <img src="./assets/images/${comment.avatar}.jpg" alt="person 1" class="is-rounded">
-  // </figure>
-  // <p>${comment.content}</p></div>`
-    const row = `<div class="columns">
-      <figure class="image is-64x64 column is-1">
-      <img src="./assets/images/${comment.avatar}.jpg" alt="person 1" class="is-rounded">
-      </figure>
-      <p>${comment.content}</p></div>
-    `
+    const row = generateRowHtml(comment)
     document.querySelector('#comments').innerHTML += row
   });
   // console.log(comments)
