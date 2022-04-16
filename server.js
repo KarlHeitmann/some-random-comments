3768602ff5f22f99f588e4f0a57dd45a7f900526
Karl Heitmann <khf1988@gmail.com>
2022-04-16 13:18:56 -0400

    created client in react, some dependencies to run both front and back using yarn, but it is not working correctly :/

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
 
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
 const PORT = process.env.PORT || 3001;
+console.log("::::::::::::: PORT, ", PORT)
 app.listen(PORT);
