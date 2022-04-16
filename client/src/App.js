7569ea93c030ab4fb1447e8846748313efa7f49b
Karl Heitmann <khf1988@gmail.com>
2022-04-16 13:27:24 -0400

    Moved section comment of V1 to the Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './css/bulma.min.css';
+import './css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="container px-2">
+      <h1 class="is-size-1">Discussion</h1>
+      <div class="is-flex is-align-items-center">
+        <figure class="image is-64x64">
+          <img src="./assets/images/person_ph_1.jpg" alt="person 1" class="is-rounded" />
+        </figure>
+        <input
+          class="input is-primary mx-4"
+          id="comment-content"
+          type="text"
+          placeholder="What are your thoughts?"
+          />
+        <div class="buttons">
+          <button class="button is-primary" id="create-comment">Comment</button>
+        </div>
+      </div>
+      <hr />
+    </section>
   );
 }
 
