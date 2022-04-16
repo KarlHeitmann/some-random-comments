e924b3136318243fcf0af1c72e454e9000d49215
Karl Heitmann <khf1988@gmail.com>
2022-04-16 13:37:58 -0400

    the comment is hooked with onChange, and button comment has a stub to handle it's click

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-// import './App.css';
+import { useState } from "react";
+import ph1 from './images/person_ph_1.jpg'
 import './css/bulma.min.css';
 import './css/style.css';
 
 function App() {
+  const [comment, setComment] = useState('')
+
+  const sendComment = (e) => {
+    console.log("sendComment", comment)
+  }
   return (
-    <section class="container px-2">
-      <h1 class="is-size-1">Discussion</h1>
-      <div class="is-flex is-align-items-center">
-        <figure class="image is-64x64">
-          <img src="./assets/images/person_ph_1.jpg" alt="person 1" class="is-rounded" />
+    <section className="container px-2">
+      <h1 className="is-size-1">Discussion</h1>
+      <div className="is-flex is-align-items-center">
+        <figure className="image is-64x64">
+          <img src={ph1} alt="person 1" className="is-rounded" />
         </figure>
         <input
-          class="input is-primary mx-4"
+          className="input is-primary mx-4"
           id="comment-content"
+          name="comment"
+          value={comment}
+          onChange={e => setComment(e.target.value)}
           type="text"
           placeholder="What are your thoughts?"
           />
-        <div class="buttons">
-          <button class="button is-primary" id="create-comment">Comment</button>
+        <div className="buttons">
+          <button
+            onClick={sendComment}
+            className="button is-primary" id="create-comment"
+            >Comment</button>
         </div>
       </div>
       <hr />
