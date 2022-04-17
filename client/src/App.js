e3cd9f5b227a9649e5864aa548ed132922c632d7
Karl Heitmann <khf1988@gmail.com>
2022-04-17 17:20:43 -0400

    added source for react

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,83 @@ import NewComment from "./components/NewComment";
 import { useEffect, useState } from 'react';
 import Comments from './components/Comments';
 import config from "./config.json"
+// small utilities that we need
+// handle json messages
+// function formatMessage = (data) => {
+const formatMessage = (data) => {
+  try {
+    const parsed = JSON.parse(data);
+    return parsed;
+  } catch (err) {
+    return data;
+  }
+};
+// get epoch timestamp
+function getTimestamp() {
+  return new Date().getTime();
+}
 
-function App() {
+
+function App({
+  retry: defaultRetry = 3,
+  retryInterval = 1500
+}) {
   const [comments, setComments] = useState([])
+  const [readyState, setReadyState] = useState(false);
+  const [send, setSend] = useState(() => () => undefined);
+  const [data, setData] = useState();
+  const [retry, setRetry] = useState(defaultRetry);
   useEffect(() => {
     axios.get(config['domain']).then(function(response) {
       const {comments} = response.data
       setComments(comments)
       // setComments(response)
     })
+    const socketUrl = 'ws://localhost:3005/?key=1'
+    const ws = new WebSocket(socketUrl);
+    ws.onopen = () => {
+      console.log('Connected to socket');
+      setReadyState(true);
+
+      // function to send messages
+      setSend(() => {
+        return (data) => {
+          try {
+            const d = JSON.stringify(data);
+            ws.send(d);
+            return true;
+          } catch (err) {
+            return false;
+          }
+        };
+      });
+
+      // receive messages
+      ws.onmessage = (event) => {
+        const msg = formatMessage(event.data);
+        const {comments} = msg
+        console.log("msg", comments)
+        console.table(comments)
+        setComments([...comments])
+        setData({ message: msg, timestamp: getTimestamp() });
+      };
+    };
+
+    // on close we should update connection state
+    // and retry connection
+    ws.onclose = () => {
+      setReadyState(false);
+      // retry logic
+      if (retry > 0) {
+        setTimeout(() => {
+          setRetry((retry) => retry - 1);
+        }, retryInterval);
+      }
+    };
+     // terminate connection on unmount
+    return () => {
+      ws.close();
+    };
   }, [])
 
   const appendComment = async (comment) => {
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require("http")
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const { comment } = require('./backend/handlers');
+const WebSocket = require('ws')
 
 const app = express();
 app.use(cors())
@@ -33,10 +35,10 @@ app.get('/', async (req, res) => {
   const {id} = req.query
   const comments = await Comment.find({parent_comment: id});
   if (id != null) {
-    console.log(id, comments)
+    // console.log(id, comments)
   } else {
-    console.log(comments)
-    console.log("asddsa")
+    // console.log(comments)
+    // console.log("asddsa")
 
   }
   res.send({comments});
@@ -54,10 +56,10 @@ app.post('/comment', async (req, res) => {
     parent_comment: id,
   })
   comment.save()
-  console.log(comment)
+  // console.log(comment)
 
-  console.log("req.body", req.body)
-  console.log("new comment")
+  // console.log("req.body", req.body)
+  // console.log("new comment")
   res.send({comment});
 });
 
@@ -73,8 +75,8 @@ app.post('/reply', async (req, res) => {
     parent_comment: id
   })
   comment.save
-  console.log(comments)
-  console.log("reply")
+  // console.log(comments)
+  // console.log("reply")
   res.send({comment});
 });
 
@@ -82,15 +84,50 @@ app.post('/upvote', async (req, res) => {
   const {id} = req.query
   // const comment = await Comment.find({_id: id});
   const comment = await Comment.findById(id);
-  console.log(comment)
+  // console.log(comment)
   comment.votes += 1
   comment.save()
-  console.log("upvote", req.query.id)
+  // console.log("upvote", req.query.id)
   res.send({comment});
 });
 
-console.log("process.env", process.env)
+const server = http.createServer(app)
+const websocketServer = new WebSocket.Server({server})
+
+//when a websocket connection is established // BORROWED FROM HERE: https://dev.to/ksankar/websockets-with-react-express-part-2-4n9f
+websocketServer.on('connection', (webSocketClient, incoming_request) => {
+  //send feedback to the incoming connection
+  // console.log(webSocketClient)
+  console.log(typeof(incoming_request.url), incoming_request.url)
+  webSocketClient.send('{ "connection" : "ok"}');
+  setInterval(async () => {
+    // const data_to_send = JSON.stringify({"msg": "vamooosss"})
+    const comments = await Comment.find({parent_comment: null})
+    console.log(comments)
+    const data_to_send = JSON.stringify({
+      "comments": comments
+    })
+    webSocketClient.send(data_to_send)
+
+  }, 1000);
+  
+  //when a message is received
+  webSocketClient.on('message', (message) => {
+
+      //for each websocket client
+      websocketServer
+      .clients
+      .forEach( client => {
+          //send the client the current message
+          client.send(`{ "message" : ${message} }`);
+      });
+  });
+});
+
+
+// console.log("process.env", process.env)
 console.log("process.env.PORT", process.env.PORT)
 const PORT = process.env.PORT || 3001;
 console.log("::::::::::::: PORT, ", PORT)
-app.listen(PORT);
+// console.log("setupWebSocket", setupWebSocket)
+server.listen(PORT);
